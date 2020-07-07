977f677137a78a88773c9e00e48e2a648c4e4300
Alice Ni <aliceni0522@gmail.com>
2020-07-07 16:44:04 +0800

    Remove Black

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.models import Page
 
 
 class BlogPage(Page):
-    
+
     description = models.CharField(max_length=255, blank=True)
 
     content_panels = Page.content_panels + [
